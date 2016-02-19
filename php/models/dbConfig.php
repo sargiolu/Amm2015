9b6006196695cebe2cb5342f0c9edb3162c7184d
Stefano <s.argiolu@yahoo.it>
2016-02-19 21:48:14 +0100

    Update dbConfig.php

--- php/models/dbConfig.php
+++ php/models/dbConfig.php
@@ -1,5 +1,14 @@
 <?php
 
+define ("DB_HOST", ""); // set database host
+
+define ("DB_USER", ""); // set database user
+
+define ("DB_PASS",""); // set database password
+
+define ("DB_NAME",""); // set database name
+/*<?php
+
 define ("DB_HOST", "localhost"); // set database host
 
 define ("DB_USER", "argioluStefano"); // set database user
@@ -8,7 +17,9 @@ define ("DB_PASS","elefante2681"); // set database password
 
 define ("DB_NAME","amm15_argioluStefano"); // set database name
 
+?>*/
 ?>
+
 
 
 
