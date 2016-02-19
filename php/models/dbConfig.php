61574f1f461147392bb2e46b3f89c2e404bb2802
Stefano <s.argiolu@yahoo.it>
2016-02-19 21:35:36 +0100

    Update dbConfig.php

--- php/models/dbConfig.php
+++ php/models/dbConfig.php
@@ -2,9 +2,9 @@
 
 define ("DB_HOST", "localhost"); // set database host
 
-define ("DB_USER", ""); // set database user
+define ("DB_USER", "argioluStefano"); // set database user
 
-define ("DB_PASS",""); // set database password
+define ("DB_PASS","elefante2681"); // set database password
 
 define ("DB_NAME","my_hellascallonisifc"); // set database name
 
